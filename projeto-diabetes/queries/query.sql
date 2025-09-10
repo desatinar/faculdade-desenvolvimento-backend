42b4434df1a2a87c7b9744506f4864dcdfcd024d
Rodrigo Santos <rodrigosantos.ati.pe@gmail.com>
2025-09-10 14:21:32 -0300

    feat: adiciona query para exibir os 10 primeiros registros da tabela pacientes

--- projeto-diabetes/queries/query.sql
+++ projeto-diabetes/queries/query.sql
@@ -1,2 +1,5 @@
 -- 1.Quais são as tabelas disponíveis no banco de dados atual?
 SELECT * FROM sqlite_master WHERE type='table';
+
+--2. Quais são os primeiros 10 registros da tabela pacientes?
+SELECT * FROM pacientes LIMIT 10;
