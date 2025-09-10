946edee2d79599cc75497b322386ca36f043941c
Rodrigo Santos <rodrigosantos.ati.pe@gmail.com>
2025-09-10 14:25:11 -0300

    feat: adiciona query para exibir nomes e tipos de dados das colunas da tabela pacientes

--- projeto-diabetes/queries/query.sql
+++ projeto-diabetes/queries/query.sql
@@ -3,3 +3,7 @@ SELECT * FROM sqlite_master WHERE type='table';
 
 --2. Quais são os primeiros 10 registros da tabela pacientes?
 SELECT * FROM pacientes LIMIT 10;
+
+--3. Quais são os nomes e tipos de dados das colunas da tabela pacientes?
+PRAGMA table_info(pacientes);
+
